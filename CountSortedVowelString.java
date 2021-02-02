8262533c9c763b0fb4b616f82456455378e56851
Reeta Wani <62195922+ReetaWani@users.noreply.github.com>
2021-02-02 10:04:51 +0530

    Create CountSortedVowelString.java

---
+++ CountSortedVowelString.java
@@ -0,0 +1,20 @@
+class CountSortedVowelString {
+
+    char ch[]=new char[]{'a','e','i','o','u'};
+    int count=0;
+    public int countVowelStrings(int n) {
+        bt(n,0);
+        return count;
+    }
+    public void bt(int n,int index){
+        if(n==0){
+            count++;
+            return;
+        }
+        for(int i=0;i<ch.length;i++){
+            if(i>=index)
+            bt(n-1,i);
+        }
+    }
+    
+}
